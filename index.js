33b71e3fc8b4cd9fcd4a36dcb4a2def3a60d8cef
Micael <micael.dossantos@edu.itescia.fr>
2021-10-22 15:55:37 +0200

    test index;Js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 var http = require('http');
 var server = http.createServer(function (request, response) {
     response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Hello World1");
+    //response.end("Hello World1");
 });
 
 app.get('/test', (req, res) => {
