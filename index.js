cbbc25b4c7b608597b88802a65ea0b7980d6ab6b
Micael <micael.dossantos@edu.itescia.fr>
2021-10-22 15:03:48 +0200

    test simple index.js

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-const express = require('express')
-const app = express()
-
 var http = require('http');
 var server = http.createServer(function (request, response) {
     response.writeHead(200, { "Content-Type": "text/plain" });
     response.end("Hello World\n");
 });
 server.listen(8000);
-
-app.get('/test', (req, res) => {
-    res.send('Hello World')
-})
