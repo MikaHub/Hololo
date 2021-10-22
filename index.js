77b6748dbc6432c64d60deed05ae5b4e6fffc266
Micael <micael.dossantos@edu.itescia.fr>
2021-10-22 16:57:17 +0200

    test index + html

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 console.log('testconsole')
+const fs = require("fs");
 const express = require('express')
 const app = express()
 var port = process.env.PORT || 3000;
@@ -9,6 +10,43 @@ console.log(process.env)
 app.get('/ping', (req, res) => {
     console.log("here")
     res.send('Hello ping')
+})
+
+app.get('/stream', (req, res) => {
+
+    // Ensure there is a range given for the video
+    const range = req.headers.range;
+    if (!range) {
+        res.status(400).send("Requires Range header");
+    }
+
+    // get video stats (about 61MB)
+    const videoPath = "https://github.com/Abdisalan/blog-code-examples/blob/master/http-video-stream/bigbuck.mp4?raw=true";
+    const videoSize = fs.statSync("https://github.com/Abdisalan/blog-code-examples/blob/master/http-video-stream/bigbuck.mp4?raw=true").size;
+
+    // Parse Range
+    // Example: "bytes=32324-"
+    const CHUNK_SIZE = 10 ** 6; // 1MB
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+
+    // Create headers
+    const contentLength = end - start + 1;
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4",
+    };
+
+    // HTTP Status 206 for Partial Content
+    res.writeHead(206, headers);
+
+    // create video read stream for this particular chunk
+    const videoStream = fs.createReadStream(videoPath, { start, end });
+
+    // Stream the video chunk to the client
+    videoStream.pipe(res);
 })
 
 app.listen(port, () => {
