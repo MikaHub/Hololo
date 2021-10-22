d0f7e4554c443aa5dc1d6c1c7940d5f2aed67c8e
Micael <micael.dossantos@edu.itescia.fr>
2021-10-22 16:12:48 +0200

    test index

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 console.log('testconsole')
 const express = require('express')
 const app = express()
-
-var http = require('http');
-var server = http.createServer(function (request, response) {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    //response.end("Serv en marche");
-    console.log('serv en marche')
-});
+var port = process.env.PORT || 3000;
 
 app.get('/test', (req, res) => {
     console.log("here")
     res.send('Hello Test')
 })
 
-var port = process.env.PORT || 3000;
-server.listen(port);
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
