c07edd27fac70ce141206935172d56eb6650d062
Micael <micael.dossantos@edu.itescia.fr>
2021-10-22 15:07:14 +0200

    update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+console.log('testconsole')
 var http = require('http');
 var server = http.createServer(function (request, response) {
     response.writeHead(200, { "Content-Type": "text/plain" });
