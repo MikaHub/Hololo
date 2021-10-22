6c3be9ddac2a3c9dace6ecab23e01ffa14ca3241
Micael <micael.dossantos@edu.itescia.fr>
2021-10-22 15:32:32 +0200

    test fist get

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 console.log('testconsole')
+const express = require('express')
+const app = express()
+
 var http = require('http');
 var server = http.createServer(function (request, response) {
     response.writeHead(200, { "Content-Type": "text/plain" });
     response.end("Hello World\n");
 });
+
+app.get('/test', (req, res) => {
+    res.send('Hello World')
+})
 
 var port = process.env.PORT || 3000;
 server.listen(port);
