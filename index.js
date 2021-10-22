f1f19c5bb9105359e97bb266086673fb01e12a61
Micael <micael.dossantos@edu.itescia.fr>
2021-10-22 15:30:25 +0200

    test .env

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ var server = http.createServer(function (request, response) {
     response.writeHead(200, { "Content-Type": "text/plain" });
     response.end("Hello World\n");
 });
-server.listen(8000);
+
+var port = process.env.PORT || 3000;
+server.listen(port);
