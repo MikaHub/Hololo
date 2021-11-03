ada86b1c7dee36c4cbd8ff4ad6730cb48c440184
Micael <micael.dossantos@edu.itescia.fr>
2021-11-03 11:24:31 +0100

    test message

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-// const fs = require("fs");
-// const fileupload = require('express-fileupload')
-// var fileUploaded = null
-// app.use(fileupload({ useTempFiles: true }))
-
+const fs = require("fs");
+const fileupload = require('express-fileupload')
+var fileUploaded = null
+app.use(fileupload({ useTempFiles: true }))
 const express = require('express')
 const { Server } = require('ws');
 //const app = express()
@@ -11,12 +10,9 @@ const { Server } = require('ws');
 
 var port = process.env.PORT || 3000
 
-//////////
 const INDEX = '/test.html';
 
-const app = express()
-    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-    .listen(port, () => console.log(`Listening on ${port}`));
+const app = express().listen(port, () => console.log(`Listening on ${port}`));
 
 const wss = new Server({ server: app });
 
@@ -27,84 +23,77 @@ wss.on('connection', (ws) => {
 
 setInterval(() => {
     wss.clients.forEach((client) => {
-        client.send(new Date().toTimeString());
+        client.send("wsh alors");
     });
 }, 1000);
 
-//////////
+var cloudinary = require('cloudinary').v2;
+cloudinary.config({
+    cloud_name: 'dhffqvijh',
+    api_key: '673912877356881',
+    api_secret: '2fdoRfjDeVZPeMMW3sk8QsuIPw8',
+});
 
+app.get("/videostreaming", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
 
-// var cloudinary = require('cloudinary').v2;
-// cloudinary.config({
-//     cloud_name: 'dhffqvijh',
-//     api_key: '673912877356881',
-//     api_secret: '2fdoRfjDeVZPeMMW3sk8QsuIPw8',
-// });
+app.get('/stream', (req, res) => {
 
-// app.get('/ping', (req, res) => {
-//     res.send('Hello ping')
-// })
+    // Ensure there is a range given for the video
+    const range = req.headers.range;
+    if (!range) {
+        res.status(400).send("Requires Range header");
+    }
 
-// app.get("/videostreaming", function (req, res) {
-//     res.sendFile(__dirname + "/index.html");
-// });
+    // get video stats (about 61MB)
+    const videoPath = "video.mp4";
+    const videoSize = fs.statSync("video.mp4").size;
 
-// app.get('/stream', (req, res) => {
+    // Parse Range
+    // Example: "bytes=32324-"
+    const CHUNK_SIZE = 10 ** 6; // 1MB
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
 
-//     // Ensure there is a range given for the video
-//     const range = req.headers.range;
-//     if (!range) {
-//         res.status(400).send("Requires Range header");
-//     }
+    // Create headers
+    const contentLength = end - start + 1;
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4",
+    };
 
-//     // get video stats (about 61MB)
-//     const videoPath = "video.mp4";
-//     const videoSize = fs.statSync("video.mp4").size;
+    // HTTP Status 206 for Partial Content
+    res.writeHead(206, headers);
 
-//     // Parse Range
-//     // Example: "bytes=32324-"
-//     const CHUNK_SIZE = 10 ** 6; // 1MB
-//     const start = Number(range.replace(/\D/g, ""));
-//     const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+    // create video read stream for this particular chunk
+    const videoStream = fs.createReadStream(videoPath, { start, end });
 
-//     // Create headers
-//     const contentLength = end - start + 1;
-//     const headers = {
-//         "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-//         "Accept-Ranges": "bytes",
-//         "Content-Length": contentLength,
-//         "Content-Type": "video/mp4",
-//     };
+    // Stream the video chunk to the client
+    videoStream.pipe(res);
+})
 
-//     // HTTP Status 206 for Partial Content
-//     res.writeHead(206, headers);
+app.post("/uploadimage", function (req, res) {
+    const file = req.files.image;
+    cloudinary.uploader.upload(file.tempFilePath, function (err, result) {
+        fileUploaded = result.url
 
-//     // create video read stream for this particular chunk
-//     const videoStream = fs.createReadStream(videoPath, { start, end });
-
-//     // Stream the video chunk to the client
-//     videoStream.pipe(res);
-// })
-
-// app.post("/uploadimage", function (req, res) {
-//     const file = req.files.image;
-//     cloudinary.uploader.upload(file.tempFilePath, function (err, result) {
-//         fileUploaded = result.url
-
-//         wsServer.on('connection', function connection(socket) {
-//             socket.send('Welcome New Client!');
-//             socket.on('message', function incoming(message) {
-//                 wsServer.clients.forEach(function (ws) {
-//                     ws.send(fileUploaded);
-//                 });
-//             });
-//         });
-//         res.send({
-//             success: true,
-//             result
-//         })
-//     });
-// })
+        wsServer.on('connection', function connection(socket) {
+            socket.send('Welcome New Client!');
+            socket.on('message', function incoming(message) {
+                wsServer.clients.forEach(function (ws) {
+                    ws.send(fileUploaded);
+                });
+            });
+        });
+        res.send({
+            success: true,
+            result
+        })
+    });
+})
 
 // app.post("/uploadvideo", function (req, res) {
 //     const file = req.files.video;
