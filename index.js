67a08e6c8a850bc4cb0ceeb1d9022fc76a175998
Micael <micael.dossantos@edu.itescia.fr>
2021-11-04 16:41:24 +0100

    socket.io enfin alleluia

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
+var port = process.env.PORT || 3030
 const fs = require("fs");
 const fileupload = require('express-fileupload')
 var fileUploaded = null
-var express = require('express');
-const http = require('http');
-const app = express();
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
 
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+// ajout de socket.io
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
 app.use(fileupload({ useTempFiles: true }))
-app.use("/static", express.static('./static'));
-var port = process.env.PORT || 3000
-
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-    });
-});
 
 var cloudinary = require('cloudinary').v2;
 cloudinary.config({
@@ -25,54 +23,30 @@ cloudinary.config({
     api_secret: '2fdoRfjDeVZPeMMW3sk8QsuIPw8',
 });
 
-app.get("/videostreaming", function (req, res) {
-    res.sendFile(__dirname + "/stream.html");
+app.get('/', (req, res) => {
+    // if (fileUploaded == null) {
+    //     res.send("Pas de upload")
+    // } else {
+    //     res.sendFile(__dirname + "/test.html");
+    // }
+
+    setInterval(function () {
+        io.emit('file', fileUploaded);
+    }, 1000);
+
+    res.sendFile(__dirname + "/index.html");
+
 });
-
-app.get('/stream', (req, res) => {
-
-    // Ensure there is a range given for the video
-    const range = req.headers.range;
-    if (!range) {
-        res.status(400).send("Requires Range header");
-    }
-
-    // get video stats (about 61MB)
-    const videoPath = "video.mp4";
-    const videoSize = fs.statSync("video.mp4").size;
-
-    // Parse Range
-    // Example: "bytes=32324-"
-    const CHUNK_SIZE = 10 ** 6; // 1MB
-    const start = Number(range.replace(/\D/g, ""));
-    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
-
-    // Create headers
-    const contentLength = end - start + 1;
-    const headers = {
-        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-        "Accept-Ranges": "bytes",
-        "Content-Length": contentLength,
-        "Content-Type": "video/mp4",
-    };
-
-    // HTTP Status 206 for Partial Content
-    res.writeHead(206, headers);
-
-    // create video read stream for this particular chunk
-    const videoStream = fs.createReadStream(videoPath, { start, end });
-
-    // Stream the video chunk to the client
-    videoStream.pipe(res);
-})
 
 app.post("/uploadimage", function (req, res) {
     const file = req.files.image;
-    setInterval(() => {
-        io.emit("file", fileUploaded);
-    }, 1000);
     cloudinary.uploader.upload(file.tempFilePath, function (err, result) {
         fileUploaded = result.url
+        io.on('connection', (socket) => {
+            console.log(`Connecté au client ${socket.id}`)
+            // émission d'un évènement
+            io.emit('file', fileUploaded)
+        })
         res.send({
             success: true,
             result
@@ -93,6 +67,11 @@ app.post("/uploadvideo", function (req, res) {
         },
         function (error, result) {
             fileUploaded = result.url
+            io.on('connection', (socket) => {
+                console.log(`Connecté au client ${socket.id}`)
+                // émission d'un évènement
+                io.emit('file', fileUploaded)
+            })
             res.send({
                 success: true,
                 result
@@ -100,14 +79,16 @@ app.post("/uploadvideo", function (req, res) {
         });
 })
 
-app.get('/get', (req, res) => {
-    if (fileUploaded == null) {
-        res.send("Pas de upload")
-    } else {
-        res.sendFile(__dirname + "/index.html");
-    }
+// établissement de la connexion
+io.on('connection', (socket) => {
+    console.log(`Connecté au client ${socket.id}`)
+    // émission d'un évènement
+    io.emit('news', 'Voici un nouvel élément envoyé par le serveur')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+
+
+// on change app par server
+server.listen(port, function () {
+    console.log(`Votre app est disponible sur localhost:${port} !`)
 })
--- static/script.js
+++
@@ -1,5 +0,0 @@
-//let socket = io();
-//io = require("socket.io-client");
-//ioClient = io.connect("http://hololo.herokuapp.com/get");
-// ioClient = io.connect("http://localhost:5000");
-// ioClient.on("file", (msg) => console.info(msg));
